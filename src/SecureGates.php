8163507d0f5ea26a44f987c0f43da0ad364569c4
Yismen Jorge <yismen.jorge@gmail.com>
2020-07-13 14:07:22 -0400

    Only users can see users dashboardq

--- src/SecureGates.php
+++ src/SecureGates.php
@@ -11,14 +11,14 @@ class SecureGates
         Gate::define(config('timy.roles.super_admin'), function ($user) { //super admin gate
             return $user->email == config('timy.super_admin_email');
         });
-        
-        Gate::define(config('timy.roles.admin'), function ($user) {//admin gate
+
+        Gate::define(config('timy.roles.admin'), function ($user) { //admin gate
             return $user->hasTimyRole(config('timy.roles.admin')) || $user->email == config('timy.super_admin_email');
         });
-        
-        Gate::define(config('timy.roles.user'), function ($user) {//user gate
-            return true; // Any user can have it
-            // return $user->hasTimyRole(config('timy.roles.user')) || $user->hasTimyRole(config('timy.roles.admin')) || $user->email == config('timy.super_admin_email');
+
+        Gate::define(config('timy.roles.user'), function ($user) { //user gate
+            // return true; // Any user can have it
+            return $user->hasTimyRole(config('timy.roles.user'));
         });
     }
 }
