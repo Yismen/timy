13009bfdf101b404b9610db738a24de2cec973b4
Yismen Jorge <yismen.jorge@gmail.com>
2020-10-21 23:52:54 -0400

    Extrated dispositions to their own view

---
+++ resources/views/_dispositions-pannel.blade.php
@@ -0,0 +1,36 @@
+<h4>Dispositions</h4>
+            <div class="card">
+                <div class="card-body m-0">
+                    <x-dc-form route="{{ route('timy_web_disposition.store') }}">
+                        @include('timy::_dispositions-form')
+                        <button type="submit" class="btn btn-primary mt-4">CREATE</button>
+                    </x-dc-form>
+                </div>
+            </div>
+
+            <div class="card mt-2">
+                <div class="card-body m-0 p-0">
+                    <table class="table table-hover table-responsive-xl w-100">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Payable</th>
+                                <th>Invoiceable</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($dispositions as $disposition)
+                                <tr>
+                                    <td>{{ $disposition->name }}</td>
+                                    <td>{{ $disposition->payable }}</td>
+                                    <td>{{ $disposition->invoiceable }}</td>
+                                    <td>
+                                        <a href="{{ route('timy_web_disposition.edit', $disposition->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
--- resources/views/dashboards/super-admin.blade.php
+++ resources/views/dashboards/super-admin.blade.php
@@ -8,45 +8,10 @@
         - Translate <br>
         - Preffer the select to change aproach on both create forced and move from Roles <br>
         <div class="col-sm-12 col-md-8">
-            <timy-super-admin-dashboard></timy-super-admin-dashboard>
+            
         </div>
         <div class="col-sm-12 col-md-4">
-            <h4>Dispositions</h4>
-            <div class="card">
-                <div class="card-body m-0">
-                    <x-dc-form route="{{ route('timy_web_disposition.store') }}">
-                        @include('timy::_dispositions-form')
-                        <button type="submit" class="btn btn-primary mt-4">CREATE</button>
-                    </x-dc-form>
-                </div>
-            </div>
-
-            <div class="card mt-2">
-                <div class="card-body m-0 p-0">
-                    <table class="table table-hover table-responsive-xl w-100">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Payable</th>
-                                <th>Invoiceable</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($dispositions as $disposition)
-                                <tr>
-                                    <td>{{ $disposition->name }}</td>
-                                    <td>{{ $disposition->payable }}</td>
-                                    <td>{{ $disposition->invoiceable }}</td>
-                                    <td>
-                                        <a href="{{ route('timy_web_disposition.edit', $disposition->id) }}" class="btn btn-warning btn-sm">Edit</a>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+            @include('timy::_dispositions-pannel')
         </div>
     </div>
 </div>
