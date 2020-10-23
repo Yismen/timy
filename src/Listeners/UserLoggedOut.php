e76881107eadde84b7f41838a43388a5c9d879ae
Yismen Jorge <yismen.jorge@gmail.com>
2020-10-23 14:22:47 -0400

    Add Event Listeners for when the user log out

---
+++ src/Listeners/UserLoggedOut.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Dainsys\Timy\Listeners;
+
+use Illuminate\Auth\Events\Logout;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class UserLoggedOut
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Logout  $event
+     * @return void
+     */
+    public function handle(Logout $event)
+    {
+        $event->user->stopRunningTimers();
+
+        $event->user->forgetTimyCache();
+    }
+}
