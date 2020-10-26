4e567c1e4ab4462944dca33c4d0b61fc8f45f742
Yismen Jorge <yismen.jorge@gmail.com>
2020-10-26 01:05:00 -0400

    Remove commented code

--- routes/api.php
+++ routes/api.php
@@ -12,16 +12,4 @@ Route::middleware($apiMiddlewares)
         Route::get('ping', 'TimerController@ping')->name('timy_ping_user');
 
         Route::apiResource('dispositions', 'DispositionController')->names('timy_dispositions');
-
-        // Route::get('super_admin', 'SuperAdminController@index')->name('timy_super_admin');
-        // Route::post('assign/{user}/{role}', 'SuperAdminController@assign')->name('timy_assign_user_role');
-        // Route::delete('unassign/{user}', 'SuperAdminController@unAssign')->name('timy_unassign_user_role');
-
-        // Route::get('timers/running', 'TimerController@running')->name('timy_timers.running');
-        // Route::post('timers/close_all', 'TimerController@closeAll')->name('timy_timers.close_all');
-        // Route::apiResource('timers', 'TimerController')->names('timy_timers');
-
-        // Route::get('admin', 'AdminController@index')->name('timy_admin');
-        // Route::post('admin/update_user_timer/{user}/{disposition}', 'AdminController@store')->name('timy_admin.create_timer_forced');
-        // Route::post('super_admin/create_forced_timer/{user}/{disposition}', 'SuperAdminController@createForcedTimer')->name('timy_super_admin.create_forced_timer');
     });
