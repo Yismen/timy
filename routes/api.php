5abd87c5ce7ed031f829cd3e768ce47a67638fb3
Yismen Jorge <yismen.jorge@gmail.com>
2020-11-04 13:56:41 -0400

    Fix typo

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,6 @@ Route::middleware($apiMiddlewares)
     ->namespace('\Dainsys\Timy\Http\Controllers\Api')
     ->group(function () {
         Route::get('/user_left/{userId}', 'TimerController@userDisconnected')->name('timy_timers.user_left');
-        Route::get('get_opent_timer_hours', 'TimerController@getOpenTimersHours')->name('timy.getOpenTimersHours');
+        Route::get('get_open_timer_hours', 'TimerController@getOpenTimersHours')->name('timy.getOpenTimersHours');
         Route::get('ping', 'TimerController@ping')->name('timy_ping_user');
     });
