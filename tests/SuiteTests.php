71004d69a3184d37ca992981ff73c12eaddb81c2
Yismen Jorge <yismen.jorge@gmail.com>
2020-11-04 21:18:10 -0400

    Test Suite

--- tests/SuiteTests.php
+++ tests/SuiteTests.php
@@ -5,17 +5,18 @@ namespace Dainsys\Timy\Tests;
 use Dainsys\Timy\Tests\Feature\ApiRotuesTestTrait;
 use Dainsys\Timy\Tests\Feature\DashboardTestTrait;
 use Dainsys\Timy\Tests\Unit\TeamsTestsTrait;
-use Dainsys\Timy\Tests\Feature\TimyDispositionTestsTrait;
 use Dainsys\Timy\Tests\Unit\CloseInactiveTimersCommandTest;
-use Dainsys\Timy\Tests\Unit\LivewireDispositionsTestsTrait;
+use Dainsys\Timy\Tests\Unit\DispositionsTestsTrait;
+use Dainsys\Timy\Tests\Unit\ForcedTimersTestsTrait;
 use Dainsys\Timy\Tests\Unit\TimeableTestTrait;
 
 class SuiteTests extends TestCase
 {
-    use DashboardTestTrait;
     use ApiRotuesTestTrait;
-    use LivewireDispositionsTestsTrait;
+    use DashboardTestTrait;
+    use DispositionsTestsTrait;
     use TeamsTestsTrait;
     use CloseInactiveTimersCommandTest;
     use TimeableTestTrait;
+    use ForcedTimersTestsTrait;
 }
