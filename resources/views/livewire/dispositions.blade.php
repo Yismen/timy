0ce4fa6160809e0162b8728c166107f67c59b116
Yismen Jorge <yismen.jorge@gmail.com>
2020-11-19 09:10:40 -0400

    Make table responsive, size sm

--- resources/views/livewire/dispositions.blade.php
+++ resources/views/livewire/dispositions.blade.php
@@ -23,29 +23,31 @@
     
     <div class="card mt-2">
         <div class="card-body m-0 p-0">
-            <table class="table table-hover table-responsive-xl w-100">
-                <thead>
-                    <tr>
-                        <th>{{ __('timy::titles.name') }}</th>
-                        <th>{{ __('timy::titles.payable') }}</th>
-                        <th>{{ __('timy::titles.invoiceable') }}</th>
-                        <th>{{ __('timy::titles.action') }}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($dispositions as $disposition)
+            <div class="table-responsive">
+                <table class="table table-hover table-sm m-0">
+                    <thead>
                         <tr>
-                            <td>{{ $disposition->name }}</td>
-                            <td>{{ $disposition->payable }}</td>
-                            <td>{{ $disposition->invoiceable }}</td>
-                            <td>
-                                <a class="btn btn-warning btn-sm" wire:click.prevent="editDisposition({{ $disposition }})">{{ __('timy::titles.edit') }}</a>
-                                {{-- <a href="{{ route('timy_web_disposition.edit', $disposition->id) }}" class="btn btn-warning btn-sm">{{ __('timy::titles.edit') }}</a> --}}
-                            </td>
+                            <th>{{ __('timy::titles.name') }}</th>
+                            <th>{{ __('timy::titles.payable') }}</th>
+                            <th>{{ __('timy::titles.invoiceable') }}</th>
+                            <th>{{ __('timy::titles.action') }}</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @foreach ($dispositions as $disposition)
+                            <tr>
+                                <td>{{ $disposition->name }}</td>
+                                <td>{{ $disposition->payable }}</td>
+                                <td>{{ $disposition->invoiceable }}</td>
+                                <td>
+                                    <a class="btn btn-warning btn-sm" wire:click.prevent="editDisposition({{ $disposition }})">{{ __('timy::titles.edit') }}</a>
+                                    {{-- <a href="{{ route('timy_web_disposition.edit', $disposition->id) }}" class="btn btn-warning btn-sm">{{ __('timy::titles.edit') }}</a> --}}
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
